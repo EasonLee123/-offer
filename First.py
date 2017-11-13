1a6ffcd6562c5805ca4eaaf750eb7e744786d41d
EasonLee123 <akroneason@gmail.com>
2017-11-13 20:28:16 +0800

    No .1 Finished

--- First.py
+++ First.py
@@ -1,5 +1,6 @@
+class Solution:
+    # array 二维列表
     def Find(self, target, array):
-        # write code here
         rownum = len(array)
         # print(rownum)                 testing purpose
         colnum = len(array[0])
