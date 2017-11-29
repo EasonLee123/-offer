b13a276f21aa841e60b149dbf91e2191c08472c3
EasonLee123 <akroneason@gmail.com>
2017-11-29 18:47:35 +0800

    No. 5 - 1
    Think from the point of view of Stack2;
    if you think from the point of view of Stack 1, then it is No.5 -2

--- Fifth_1.py
+++ Fifth_1.py
@@ -6,15 +6,21 @@ class Solution:
 	def queue_push(self,node):
 		self.stack1.append(node)
 
-	def queue_pop(self):
-		if len(self.stack2) == 0 and len(self.stack1 == 0):
+	# Think from the point of view of stack2 to create queue_pop function
+	def queue_pop(self):    
+		if len(self.stack2) == 0 and len(self.stack1) == 0:
 			print("Nothing to pop")
 
 		elif len(self.stack2) == 0:
 			while len(self.stack1):
 				self.stack2.append(self.stack1.pop())  # pop() by default is pop(-1)
+			return self.stack2.pop()
 
-		return self.stack2.pop()
+		elif len(self.stack2) !=0:
+			return self.stack2.pop()
+		
+
+
 
 x = Solution()
 
@@ -27,7 +33,7 @@ print(x.queue_pop())
 x.queue_push(4)
 
 print(x.queue_pop())
+print(x.queue_pop())
+print(x.queue_pop())
 
 
-
-		
